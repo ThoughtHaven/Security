4434e887c37358602de6ef8b68f0fbd734dae014
Brendan Ridenour <Journeyman979@hotmail.com>
2018-03-05 10:26:34 -0500

    Commit random

---
+++ src/ThoughtHaven.Security.Cryptography.Random/CryptoRandom.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ThoughtHaven.Security.Cryptography
+{
+    public class CryptoRandom
+    {
+        private const string Zero = "0";
+        private const string Numbers = "1234567890";
+        private static readonly long MaxValueLength = long.MaxValue.ToString().Length;
+
+        public virtual long GenerateNumber(int length)
+        {
+            if (length > MaxValueLength)
+            {
+                throw new ArgumentOutOfRangeException(
+                    paramName: nameof(length),
+                    message: $"The {nameof(length)} argument's value of {length} would require a number larger than the {nameof(Int64)}.{nameof(long.MaxValue)}.");
+            }
+
+            var randomString = this.GenerateString(length, Numbers);
+
+            while (randomString.StartsWith(Zero))
+            {
+                var next = this.GenerateString(1, Numbers);
+                randomString = $"{randomString.Substring(1)}{next}";
+            }
+
+            return long.Parse(randomString);
+        }
+
+        public virtual string GenerateString(int length) =>
+            this.GenerateString(length, characterSet: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+
+        public virtual string GenerateString(int length, string characterSet)
+        {
+            if (length < 1)
+            {
+                throw new ArgumentOutOfRangeException(paramName: nameof(length), message: $"The {nameof(length)} argument cannot be less than 1.");
+            }
+
+            if (characterSet == null)
+            {
+                throw new ArgumentNullException(nameof(characterSet));
+            }
+
+            if (string.IsNullOrWhiteSpace(characterSet))
+            {
+                throw new ArgumentException(paramName: nameof(characterSet), message: $"The {nameof(characterSet)} argument cannot be null or white space.");
+            }
+
+            if (characterSet.Length < 2)
+            {
+                throw new ArgumentException(paramName: nameof(characterSet), message: $"The {nameof(characterSet)}.{nameof(characterSet.Length)} must be at least 2.");
+            }
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var bytes = new byte[length];
+
+                rng.GetBytes(bytes);
+
+                var result = new char[length];
+                while (length-- > 0)
+                {
+                    result[length] = characterSet[bytes[length] % characterSet.Length];
+                }
+
+                return new string(result);
+            }
+        }
+    }
+}
---
+++ test/ThoughtHaven.Security.Cryptography.Random.Test/CryptoRandomTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Xunit;
+
+namespace ThoughtHaven.Security.Cryptography
+{
+    public class CryptoRandomTests
+    {
+        private static readonly Random _random = new Random();
+
+        public class GenerateNumberMethod
+        {
+            public class LengthOverload
+            {
+                [Fact]
+                public void ValidLength_ReturnsIntOfCorrectLength()
+                {
+                    var length = _random.Next(3, long.MaxValue.ToString().Length);
+                    var random = new CryptoRandom();
+
+                    var result = random.GenerateNumber(length);
+
+                    Assert.Equal(length, result.ToString().Length);
+                }
+
+                [Fact]
+                public void LengthLessThanOne_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentOutOfRangeException>("length", () =>
+                    {
+                        random.GenerateNumber(length: 0);
+                    });
+                }
+
+                [Fact]
+                public void LengthLargerThanInt64MaxValue_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentOutOfRangeException>("length", () =>
+                    {
+                        random.GenerateNumber(length: long.MaxValue.ToString().Length + 1);
+                    });
+                }
+            }
+        }
+
+        public class GenerateString
+        {
+            public class LengthOverload
+            {
+                [Fact]
+                public void ValidLength_ReturnsStringOfCorrectLength()
+                {
+                    var length = _random.Next(5, 100);
+                    var random = new CryptoRandom();
+
+                    var result = random.GenerateString(length);
+
+                    Assert.Equal(length, result.Length);
+                }
+
+                [Fact]
+                public void LengthLessThanOne_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentOutOfRangeException>("length", () =>
+                    {
+                        random.GenerateString(length: 0);
+                    });
+                }
+            }
+
+            public class LengthAndCharacterSetOverload
+            {
+                [Fact]
+                public void ValidArgs_ReturnsStringOfCorrectLengthWithExpectedCharacters()
+                {
+                    var length = _random.Next(5, 100);
+                    var characterSet = "anpz370";
+                    var random = new CryptoRandom();
+
+                    var result = random.GenerateString(length, characterSet);
+
+                    Assert.Equal(length, result.Length);
+
+                    foreach (var character in result)
+                    {
+                        Assert.Contains(character.ToString(), characterSet);
+                    }
+                }
+
+                [Fact]
+                public void LengthLessThanOne_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentOutOfRangeException>("length", () =>
+                    {
+                        random.GenerateString(length: 0, characterSet: "abc");
+                    });
+                }
+
+                [Fact]
+                public void NullCharacterSet_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentNullException>("characterSet", () =>
+                    {
+                        random.GenerateString(length: 5, characterSet: null);
+                    });
+                }
+
+                [Fact]
+                public void EmptyCharacterSet_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentException>("characterSet", () =>
+                    {
+                        random.GenerateString(length: 5, characterSet: string.Empty);
+                    });
+                }
+
+                [Fact]
+                public void WhiteSpaceCharacterSet_Throws()
+                {
+                    var random = new CryptoRandom();
+                    
+                    Assert.Throws<ArgumentException>("characterSet", () =>
+                    {
+                        random.GenerateString(length: 5, characterSet: " ");
+                    });
+                }
+
+                [Fact]
+                public void CharacterSetUnderTwoCharactersLong_Throws()
+                {
+                    var random = new CryptoRandom();
+
+                    Assert.Throws<ArgumentException>("characterSet", () =>
+                    {
+                        random.GenerateString(length: 5, characterSet: "a");
+                    });
+                }
+            }
+        }
+    }
+}
